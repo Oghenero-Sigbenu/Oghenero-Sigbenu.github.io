ed0730d481d203190883596a4f29212259eca731
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2019-04-01 06:57:54 +0100

    changes made

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    const img = $('img');
+    const img  = $('#image-slide');
     const next = $(".next");
     const prev = $(".prev");
+    const hamb = $(".hamb-menu");
+    const nav  =  $(".nav");
+   
     //images to be displayed in an array
     const images = ["./images/Capture.PNG","./images/Foody UI.PNG", "./images/Creative UI.PNG"];
 
@@ -15,25 +18,25 @@ $(document).ready(function() {
         if(imagesIndex < imageLength - 1){
             imagesIndex++
             img.attr("src", images[imagesIndex])
-        }else{
+        } 
+        else{
             imagesIndex === imageLength - 1
             imagesIndex = 0
                 img.attr("src", images[imagesIndex]);
+                next.disabled = false;
         }
-        // setInterval(next, 3000);
+        setInterval(next, 3000);
     });
 
-//previous button
-// prev.click(() =>{
-//     // console.log("love")
-//     if(imagesIndex > imageLength - 1){
-//         imagesIndex --
-//         img.attr("src", images[imagesIndex])
-//     }else{
-//         imagesIndex === imageLength
-//         imagesIndex = 0
-//             img.attr("src", images[imagesIndex]);
-//     }
+    //previous button
+    prev.click(() =>{
+    if(imagesIndex < 0){
+        imagesIndex -= 1
+        img.attr("src", images[imagesIndex])
+    }
 });
 
-
+    hamb.click(() =>{
+        console.log("lobe");
+    })
+});

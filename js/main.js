23e3362c313496f6d56e34c08634221a784f3f58
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2019-04-10 13:47:30 +0100

    update commit

--- js/main.js
+++ js/main.js
@@ -2,41 +2,28 @@ $(document).ready(function() {
     const img  = $('#image-slide');
     const next = $(".next");
     const prev = $(".prev");
-    const hamb = $(".hamb-menu");
-    const nav  =  $(".nav");
    
     //images to be displayed in an array
     const images = ["./images/Capture.PNG","./images/Foody UI.PNG", "./images/Creative UI.PNG"];
-
-    let imagesIndex;
+    let imagesIndex = 0;
     //The length of the array
     const imageLength = images.length;
 
     //onclicking on the next botton tbe images should change
     next.click(() =>{
-        // console.log("love")
         if(imagesIndex < imageLength - 1){
             imagesIndex++
             img.attr("src", images[imagesIndex])
-        } 
-        else{
+        } else{
             imagesIndex === imageLength - 1
             imagesIndex = 0
-                img.attr("src", images[imagesIndex]);
-                next.disabled = false;
-        }
-        setInterval(next, 3000);
-    });
+            img.attr("src", images[imagesIndex]);
+                                                }});
 
-    //previous button
-    prev.click(() =>{
-    if(imagesIndex < 0){
-        imagesIndex -= 1
-        img.attr("src", images[imagesIndex])
-    }
-});
+    setInterval(() => {
+    if(imagesIndex < imageLength - 1){
+        imagesIndex++
+        img.attr("src", images[imagesIndex])} 
+        }, 2000);
 
-    hamb.click(() =>{
-        console.log("lobe");
-    })
 });

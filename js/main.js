36d3e96d54074f6ffef9a1d900e131ccd9892b5c
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2019-06-06 15:22:44 +0100

    changed design

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-    const img  = $('#image-slide');
-    const next = $(".next");
-    const prev = $(".prev");
+// $(document).ready(function() {
+//     const img  = $('#image-slide');
+//     const next = $(".next");
+//     const prev = $(".prev");
    
-    //images to be displayed in an array
-    const images = ["./images/Capture.PNG","./images/Foody UI.PNG", "./images/Creative UI.PNG"];
-    let imagesIndex = 0;
-    //The length of the array
-    const imageLength = images.length;
+//     //images to be displayed in an array
+//     const images = ["./images/Capture.PNG","./images/Foody UI.PNG", "./images/Creative UI.PNG"];
+//     let imagesIndex = 0;
+//     //The length of the array
+//     const imageLength = images.length;
 
-    //onclicking on the next botton tbe images should change
-    next.click(() =>{
-        if(imagesIndex < imageLength - 1){
-            imagesIndex++
-            img.attr("src", images[imagesIndex])
-        } else{
-            imagesIndex === imageLength - 1
-            imagesIndex = 0
-            img.attr("src", images[imagesIndex]);
-                                                }});
+//     //onclicking on the next botton tbe images should change
+//     next.click(() =>{
+//         if(imagesIndex < imageLength - 1){
+//             imagesIndex++
+//             img.attr("src", images[imagesIndex])
+//          } else{
+//             imagesIndex === imageLength - 1
+//             imagesIndex = 0
+//             img.attr("src", images[imagesIndex]);
+//                                                 }});
 
-    setInterval(() => {
-    if(imagesIndex < imageLength - 1){
-        imagesIndex++
-        img.attr("src", images[imagesIndex])} 
-        }, 2000);
+//     setInterval(() => {
+//     if(imagesIndex < imageLength - 1){
+//         imagesIndex++
+//         img.attr("src", images[imagesIndex])} 
+//         }, 2000);
 
-});
+// });
